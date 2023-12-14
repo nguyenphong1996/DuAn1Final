1d7498824eb0487a0927a55e071b2899e7f0097f
xNoEndx <zicuteo123@gmail.com>
2023-12-14 16:14:49 +0700

    lan2

---
+++ app/src/main/java/com/example/afinal/ImageViewButton/TraCuuActivity.java
@@ -0,0 +1,16 @@
+package com.example.afinal.ImageViewButton;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.duan.R;
+
+public class TraCuuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tra_cuu);
+    }
+}
